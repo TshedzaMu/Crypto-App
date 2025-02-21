fe8d4e56a6bf99bd255bfe17cfdefc487dc52406
Tshedza Musandiwa <>
2025-02-21 18:19:51 +0200

    Initial commit

---
+++ Cypto Life/Cypto Life/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cypto Life
+//
+//  Created by Tshedza Musandiwa on 2025/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
