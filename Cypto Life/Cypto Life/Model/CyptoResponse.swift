cdc4174fa1d1265e4155a824f745288b0ae4b008
Tshedza Musandiwa <>
2025-02-22 12:38:36 +0200

    Updated crypto response mode

--- Cypto Life/Cypto Life/Model/CyptoResponse.swift
+++ Cypto Life/Cypto Life/Model/CyptoResponse.swift
@@ -7,6 +7,46 @@
 
 import Foundation
 
-class CyptoResponse:Codable {
-    
+struct CryptoResponse: Codable {
+    let data: CryptoData
+}
+
+struct CryptoData: Codable {
+    let stats: CryptoStats
+    let coins: [CryptoCoin]
+}
+
+struct CryptoStats: Codable {
+    let total: Int
+    let totalCoins: Int
+    let totalMarkets: Int
+    let totalExchanges: Int
+    let totalMarketCap: String
+    let total24hVolume: String
+}
+
+struct CryptoCoin: Codable {
+    let uuid: String
+    let symbol: String
+    let name: String
+    let color: String
+    let iconUrl: String
+    let marketCap: String
+    let price: String
+    let listedAt: Int
+    let tier: Int
+    let change: String
+    let rank: Int
+    let sparkline: [String?]
+    let lowVolume: Bool
+    let coinrankingUrl: String
+    let volume24h: String
+    let btcPrice: String
+    let contractAddresses: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case uuid, symbol, name, color, iconUrl, marketCap, price, listedAt, tier, change, rank, sparkline, lowVolume, coinrankingUrl
+        case volume24h = "24hVolume"
+        case btcPrice, contractAddresses
+    }
 }
